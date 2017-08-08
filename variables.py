ab6e5754283999ece4e77da959c6f9c868b964a7
Unknown <remilavedrine-git@yahoo.fr>
2017-08-08 11:23:36 +0200

    Add Security Manager information
    Add Security Manager information as a module variables on the "variables.py" file so that it is available to every modules.

--- variables.py
+++ variables.py
@@ -7,3 +7,7 @@ NOT_EVALUATED_PHASE = 'Not Evaluated'
 NOT_STARTED_PHASE = 'Not Started'
 IN_PROGRESS_PHASE = 'In Progress'
 DONE_PHASE = 'Done'
+
+SECURITY_MANAGER_NAME = 'Rémi Lavedrine'
+SECURITY_MANAGER_EMAIL = 'remi.lavedrine@orange.com'
+SECURITY_MANAGER_PHONE = '06 31 17 80 39'
